031f7b8314851e1269eec64b1f451ec18f2521f0
Aaditya Agrawal <aaditya1106@gmail.com>
2023-04-04 23:14:33 +0530

    Updated raft.py

--- manager/src/raft.py
+++ manager/src/raft.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Union
+import uuid
 
 from pysyncobj import SyncObj
 from pysyncobj.batteries import ReplDict
@@ -60,8 +61,74 @@ def is_leader() -> bool:
     return get_raft_status().get('is_leader', False)
 
 
-def add_broker(id:int, broker_addr: str) -> bool:
+def add_broker(broker_addr: str) -> bool:
     broker_dict = get_broker_dict()
-    if id in broker_dict:
-        raise Exception(f'Broker with id {id} already exists')
-    broker_dict[id] = broker_addr
+    if broker_addr in broker_dict.values():
+        raise Exception(f'Broker with id {broker_addr} already exists')
+    
+    broker_id = str(uuid.uuid4().hex)
+    broker_dict[broker_id] = broker_addr
+
+# Functions for topic_dict
+def add_topic(topic_name: str):
+    topic_dict = get_topic_dict()
+    if topic_name in topic_dict:
+        raise Exception(f'Topic {topic_name} already exists')
+    
+    topic_dict[topic_name] = {}
+
+def add_broker_to_topic_partition(topic_name: str, partition_id: int, broker_id: str):
+    topic_dict = get_topic_dict()
+
+    if topic_name not in topic_dict:
+        raise Exception(f'Topic {topic_name} does not exist')
+    
+    if partition_id not in topic_dict[partition_id]:
+        topic_dict[topic_name][partition_id] = set()
+
+    if broker_id in topic_dict[topic_name][partition_id]:
+        raise Exception(f'Broker id {broker_id} already added for topic name {topic_name}.')
+    
+    topic_dict[topic_name][partition_id].add(broker_id)
+
+# Functions for producer_dict
+def add_producer(topic_name: str) -> str:
+    topic_dict = get_topic_dict()
+
+    if topic_name not in topic_dict:
+        add_topic(topic_name)
+
+    producer_dict = get_producer_dict()
+
+    producer_id = str(uuid.uuid4().hex)
+    producer_dict[producer_id] = topic_name
+
+    return producer_id
+
+def is_producer(producer_id: str, topic_name: str) -> bool:
+    producer_dict = get_producer_dict()
+
+    if producer_id not in producer_dict or producer_dict[producer_id] != topic_name:
+        return False    
+    return True
+
+# Functions for consumer_dict
+def add_consumer(topic_name: str) -> str:
+    topic_dict = get_topic_dict()
+
+    if topic_name not in topic_dict:
+        raise Exception(f'Topic {topic_name} does not exist')
+
+    consumer_dict = get_consumer_dict()
+
+    consumer_id = str(uuid.uuid4().hex)
+    consumer_dict[consumer_id] = topic_name
+    return consumer_id
+
+def is_consumer(consumer_id: str, topic_name: str) -> bool:
+    consumer_dict = get_consumer_dict()
+
+    if consumer_id not in consumer_dict or consumer_dict[consumer_id] != topic_name:
+        return False    
+    return True
+
